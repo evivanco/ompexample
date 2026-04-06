ec3c1ee2ef396c2897afe42311b2b592819aaca9
Ernesto Vivanco <eduardo.vivanco@gmail.com>
2026-04-06 11:04:28 -0400

    Cuarto branch

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
     clock_t inicio, fin;
     double time_ms;
     int i;
-    int n = 12;
+    int n = 1200;
     inicio = clock();
     @pragma omp parallel 
     for (i = 0; i < n; i++)
