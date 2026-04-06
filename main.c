7b21e078201a16cfff3f839e9d7cbd73d4c3b2b6
Ernesto Vivanco <eduardo.vivanco@gmail.com>
2026-04-06 09:29:11 -0400

    Segundo branch

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <omp.h>
+#include <time.h>
 int main() {
+    clock_t inicio, fin;
+    double time_ms;
     int i;
-    int n = 10;
-
+    int n = 100;
+    inicio = clock();
     @pragma omp parallel 
     for (i = 0; i < n; i++)
     {
         int thread = omp_get_thread_num();
         printf("Iteracion %d con thread %d\n", i, thread);
     }
+    fin = clock();
+    time_ms = ((double)(fin - inicio)/CLOCKS_PER_SEC)*1000;
+    printf("t(ms) : %.8f", time_ms)
     return 0;
 }
